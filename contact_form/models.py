fe74965c47fc8a4062ee8f9978981ec619d7d924
Faisal Mehmood Awan <96324075+FaisalAwa@users.noreply.github.com>
2024-11-01 23:14:05 -0700

    Add files via upload

---
+++ contact_form/models.py
@@ -0,0 +1,12 @@
+# contact_form/models.py
+
+from django.db import models
+
+class ContactFormEntry(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
